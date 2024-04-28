11908e62e9c16a5b5900030a43bc2d55dea8d1bb
princesinghrajput <psr8084@gmail.com>
2024-04-28 09:25:13 +0530

    Implemented validations

--- models_play/models/models.py
+++ models_play/models/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+from django.core.validators import(
+    EmailValidator,
+    MaxValueValidator,
+    MinValueValidator,
+    validate_slug
+)
+
+
+
+
 # Create your models here.
 
 
@@ -7,6 +17,53 @@ from django.db import models
 # every row in the database is represented by an object of the class
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    roll_number = models.IntegerField()
+
+    GENDERS = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+        ('o', 'Other')
+    )
+
+
+
+    # varChar(100)
+    name = models.CharField(max_length=100, null=True)
+
+    # integer
+    roll_number = models.IntegerField(max_length=100, null=True)
+
+    roll_number= models.IntegerField(unique=True, null=True)
+
+    # Text
+    #Can be null in DB
+    # can not be null in ORM LEVEL
+
+    address = models.TextField(null=True)
+
+    phone_number = models.CharField(max_length=15 , null=True, blank=True)
+
+    #email field is nothing but varChar(255)
+    email= models.EmailField(max_length=25, null=True, validators=[EmailValidator("Email address is not valid")])
+
+    gender = models.CharField(max_length=1, choices=GENDERS, null=True)
     
+
+
+    age = models.IntegerField(
+        null=True,
+        
+        validators=[
+            MaxValueValidator(150),
+            MinValueValidator(0),
+
+        ]
+
+        )
+
+
+    slug = models.CharField(max_length=100, null=True, validators=[validate_slug])
+    
+    #to show the name of models only in admin panel
+
+    def __str__(self):
+        return self.name
